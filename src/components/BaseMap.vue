7abdb92196ddde5bc0c0c3c7ff9a7d2cfedc457f
amirpeace1080 <amirpeace1080@gmail.com>
2022-01-18 21:03:33 +0330

    set map

---
+++ src/components/BaseMap.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="mapContainer" class="basemap"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+
+export default {
+  name: "BaseMap",
+  data() {
+    return {
+      accessToken:
+        "pk.eyJ1IjoiYW1pcnBlYWNlMTA4MCIsImEiOiJja3lrYXExaWkwYWRzMnZwZXU0bWRlbzVkIn0.ot6KCZlh8EH4PjMA_146kg",
+    };
+  },
+  mounted() {
+    mapboxgl.accessToken = this.accessToken;
+
+    new mapboxgl.Map({
+      container: "mapContainer",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [103.811279, 1.345399],
+      zoom: 12,
+      maxBounds: [
+        [103.6, 1.1704753],
+        [104.1, 1.4754753],
+      ],
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.basemap {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="map" />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { onMounted } from "vue";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    onMounted(() => {
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoiYW1pcnBlYWNlMTA4MCIsImEiOiJja3lrYXExaWkwYWRzMnZwZXU0bWRlbzVkIn0.ot6KCZlh8EH4PjMA_146kg";
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/light-v9",
+      });
+      map.on('load', () => {
+      // TODO: Here we want to load a layer
+      // TODO: Here we want to load/setup the popup
+      });
+    });
+    return {};
   },
 };
 </script>
+
+<style>
+#map {
+  height: 100vh;
+}
+</style>
